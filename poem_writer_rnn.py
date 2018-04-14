e4d14f2ea8422ae24ed29e6352490744df329abb
Ke Liu <lkforward@gmail.com>
2018-04-13 21:42:41 -0500

    Add callback function to generate text.

--- poem_writer_rnn.py
+++ poem_writer_rnn.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-A generative model trained to write poems using RNN. 
+A generative model trained to write poems using RNN.
+Reference: https://github.com/keras-team/keras/blob/master/examples/lstm_text_generation.py.  
 
 Created on Sun Apr 08 18:14:34 2018
 
 @author: kliu14
 """
-
 import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.layers import LSTM
+from keras.callbacks import LambdaCallback
+from keras.optimizers import RMSprop
+
+import sys
 
 from utils import load_poems
+
 
 def char_seq(fname):
     # Read the sonnets into a sequence of characters. 
@@ -69,6 +74,68 @@ def preprocess_text(fname, seq_length):
         train_y[i, char_to_indices[next_char[i]]] = 1
     
     return train_x, train_y, indices_to_char
+    
+    
+def test_preprocess():
+    fname = r"E:\Coursera\CS155\LiuKe_hw_submission\prj3_code\data\shakespeare.txt"
+    train_x, train_y, _ = preprocess_text(fname, 7)
+    
+    print("The first training sequence: ", train_x[0, :, :])
+    print("The first training label:", train_y[0, :])
+    
+    return None    
+    
+    
+def sampling(preds, temperature):
+    # Sample a character according to the distribution predicted by the softmax function
+    # in RNN 
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    
+    return np.argmax(probas)
+
+
+def on_epoch_end(epoch, logs):
+    # Collect things to output at the end of an epoch. 
+    
+    print("")
+    print('----- Generating text after Epoch: %d' % epoch)
+    
+    temperature = 1.0
+    # Here we start from a random position between 0 and 100.
+    # start_index = np.random.randint(0, 100)
+    start_index = 0
+    num_words_to_generate = 400
+    
+    # Get a sequence to start with:
+    generated = ''
+    sentence = ''.join(sonnets_in_char[start_index:start_index+seq_length])
+    generated += sentence
+    
+    # Make a prediction based on the trained model after this epoch: 
+    for i in range(num_words_to_generate):
+        # Make a vector for the inputs: 
+        x_pred = np.zeros((1, seq_length, len(chars)))
+        for i in range(seq_length):
+            x_pred[0, i, char_to_indices[sentence[i]]] = 1
+        
+        # Make a prediction using the trained model: 
+        prds = model.predict(x_pred, verbose=0)[0]
+        prd_index = sampling(prds, temperature)
+        # Convert the model prediction into a character
+        next_char = indices_to_char[prd_index]
+        
+        generated += next_char
+        # Merge the prediction into the inputs for the next prediction: 
+        sentence = sentence[1:] + next_char
+        
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
+
+    print()
 
 
 def build_rnn(num_unit, seq_length, input_dim):
@@ -101,8 +168,51 @@ def training_data():
 
 
 if __name__ == "__main__":
-    check_rnn_parameters()
+    # check_rnn_parameters()
+    # test_preprocess()
 
-#    fname = r"E:\Coursera\CS155\LiuKe_hw_submission\prj3_code\data\shakespeare.txt"
-#    seq_length = 40 
-#    preprocess_text(fname, seq_length)
+    # ======================================================
+    # Function 1
+    # ======================================================
+    fname = r"E:\Coursera\CS155\LiuKe_hw_submission\prj3_code\data\shakespeare.txt"
+    
+    seq_length = 40
+    
+    sonnets_in_char = char_seq(fname)
+    print(sonnets_in_char[:30])
+    
+    chars = char_vocab(sonnets_in_char)
+    print(chars[:5])    
+    char_to_indices = dict((char,i) for i, char in enumerate(chars))
+    indices_to_char = dict((i, char) for i, char in enumerate(chars))
+    
+    # Make sonnets into input data of specified shape
+    # a. Make sequences of chars into the format of X, y
+    seqs = [] 
+    next_char = [] 
+    for i in range(len(sonnets_in_char) - seq_length):
+        seqs.append(sonnets_in_char[i:i+seq_length])
+        next_char.append(sonnets_in_char[i+seq_length])
+    
+    # b. Convert the chars into numbers: 
+    train_x = np.zeros((len(seqs), seq_length, len(chars)))
+    train_y = np.zeros((len(seqs), len(chars)))
+    
+    for i, sentence in enumerate(seqs):
+        for j, single_char in enumerate(sentence):
+            train_x[i, j, char_to_indices[single_char]] = 1
+        train_y[i, char_to_indices[next_char[i]]] = 1
+        
+    
+    # ======================================================
+    # Function 2: Train RNN
+    # ====================================================== 
+    num_unit = 100
+    input_dim = len(chars)   
+    model = build_rnn(num_unit, seq_length, input_dim) 
+    
+    optimizer = RMSprop(lr=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+    
+    print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+    model.fit(train_x, train_y, batch_size=128, epochs=60, callbacks=[print_callback])
